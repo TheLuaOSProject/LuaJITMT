8c56f412dde143514e5556d4e1d7425f261758d7
Amrit Bhogal <ambhogal01@gmail.com>
2026-06-28 03:41:43 -0700

    tests: skip ffi ldscript lane on macos

--- tests/suites/m7_ffi.lua
+++ tests/suites/m7_ffi.lua
@@ -250,6 +250,10 @@ fi
     name = "m7_ffi_clib_ldscript",
     description = "FFI C library GNU ld-script resolution behavior",
     run = function(t)
+      if jit.os == "OSX" then
+        print("M7 FFI clib ld-script behavior skipped on macOS")
+        return
+      end
       clean_build(t)
       local script = build_clib_ldscript_fixture(t)
       luajit_code(t, [[
