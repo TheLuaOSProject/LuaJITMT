57cc5b87c52db962e1fa93323766a05d18a3889d
Amrit Bhogal <ambhogal01@gmail.com>
2026-06-30 23:38:22 +0000

    types: tighten threading gcstats annotations

--- etc/threading.lua
+++ etc/threading.lua
@@ -6,6 +6,107 @@
 ---@alias threading.peek_status true|false
 ---@alias threading.send_result true|nil
 ---@alias threading.gcmode "incremental"|"generational"
+---@alias threading.gcstats_latency_buckets table<integer, number>
+
+---@class threading.gcstats
+---@field [string] number|threading.gcstats_latency_buckets
+---@field total_bytes number
+---@field total_kbytes number
+---@field phase integer
+---@field generational integer
+---@field cycle_minor_requested integer
+---@field cycle_sweep_minor integer
+---@field minor_sweep_enabled integer
+---@field cycle_roots_minor integer
+---@field minor_roots_enabled integer
+---@field cycle_requests number
+---@field cycle_starts number
+---@field major_cycle_starts number
+---@field minor_cycle_requests number
+---@field minor_cycle_starts number
+---@field minor_sweep_deferred number
+---@field minor_sweep_arenas number
+---@field minor_roots_deferred number
+---@field major_root_scans number
+---@field minor_root_scans number
+---@field minor_survival_base_live number
+---@field minor_survival_bytes number
+---@field minor_survival_pct integer
+---@field minor_survival_threshold_pct integer
+---@field minor_survival_major_requests number
+---@field remembered_barriers number
+---@field remembered_pushed number
+---@field remembered_overflows number
+---@field remembered_filtered number
+---@field remembered_drained number
+---@field poll_ack_samples number
+---@field poll_ack_latency_sum_ns number
+---@field poll_ack_latency_max_ns number
+---@field poll_ack_latency_buckets threading.gcstats_latency_buckets
+---@field alloc_since_trigger number
+---@field cycle_alloc_bytes number
+---@field trigger_bytes number
+---@field hard_bytes number
+---@field assist_runs number
+---@field assist_grey_drained number
+---@field assist_ssb_converted number
+---@field assist_weak_drained number
+---@field worker_runs number
+---@field worker_grey_drained number
+---@field worker_ssb_converted number
+---@field worker_weak_drained number
+---@field worker_idle_declares number
+---@field worker_busy_retries number
+---@field worker_wakes number
+---@field worker_parks number
+---@field worker_async_progress number
+---@field sweep_owner_runs number
+---@field sweep_owner_arenas number
+---@field sweep_owner_live_cells number
+---@field sweep_live_updates number
+---@field sweep_live_huge_bytes number
+---@field live_estimate number
+---@field weak_clear_tables number
+---@field weak_clear_cleared number
+---@field weak_bridge_skipped number
+---@field weak_bridge_fallbacks number
+---@field weak_bridge_backfills number
+---@field weak_bridge_backfill_tables number
+---@field weak_bridge_backfill_slots number
+---@field weak_bridge_backfill_cleared number
+---@field weak_keys_marked number
+---@field weak_values_marked number
+---@field finreg_cdata_sets number
+---@field finreg_cdata_clears number
+---@field finreg_cdata_queued number
+---@field finreg_cdata_sweep_queued number
+---@field finreg_cdata_pweak_queued number
+---@field finreg_cdata_pweak_claimed number
+---@field finreg_cdata_preclaim_overflow number
+---@field finreg_cdata_preclaim_dispatched number
+---@field finreg_cdata_order_seen number
+---@field finreg_cdata_order_claimed number
+---@field finreg_cdata_order_unlinked number
+---@field finreg_cdata_order_queued number
+---@field finreg_cdata_order_retired number
+---@field finreg_cdata_order_tombstones number
+---@field finreg_cdata_order_fallbacks number
+---@field finreg_cdata_pending_order_hits number
+---@field finreg_udata_sets number
+---@field finreg_udata_clears number
+---@field finreg_udata_queued number
+---@field finreg_udata_registered number
+---@field finreg_udata_retired_nodes number
+---@field finreg_udata_discovered number
+---@field finreg_udata_forgets number
+---@field finalizer_queued number
+---@field finalizer_dequeued number
+---@field finalizer_mpsc_drained number
+---@field finalizer_enters number
+---@field finalizer_leaves number
+---@field finalizer_sweep_blocks number
+---@field finalizer_spawn_deferrals number
+---@field finalizer_spawn_release_wakes number
 
 ---@class threading.thread: userdata
 local threading_thread = {}
@@ -135,7 +236,7 @@ function threading.fence() end
 function threading.sleep(seconds) end
 
 ---Return GC2/lockless runtime telemetry used by tests and benchmarks.
----@return table<string, number|boolean|table> stats
+---@return threading.gcstats stats
 ---@nodiscard
 function threading.gcstats() end
 
